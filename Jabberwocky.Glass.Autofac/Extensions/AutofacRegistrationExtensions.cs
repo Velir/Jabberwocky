4cc22c0ce25f9fde9ea8674502c25040d2986087
Chris Smith <chris.smith@velir.com>
2014-11-25 13:53:44 -0500

    Updating comment

--- Jabberwocky.Glass.Autofac/Extensions/AutofacRegistrationExtensions.cs
+++ Jabberwocky.Glass.Autofac/Extensions/AutofacRegistrationExtensions.cs
@@ -5,6 +5,15 @@ namespace Jabberwocky.Glass.Autofac.Extensions
 {
 	public static class AutofacRegistrationExtensions
 	{
+
+		/// <summary>
+		/// Enables the root container to be resolvable, and available for injection
+		/// </summary>
+		/// <remarks>
+		/// It is imperative that this is called after the container is built, in order for the Glass Interface Factory to work
+		/// </remarks>
+		/// <param name="container"></param>
+		/// <returns></returns>
 		public static IContainer RegisterContainer(IContainer container)
 		{
 			// Enable resolving the root container
