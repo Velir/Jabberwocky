f62afcba09f3f1d3c0912842ed0ba976489399c5
Chris Smith <chris.smith@velir.com>
2015-03-13 12:51:57 -0400

    Implementing Kevin's MVC GET/POST custom attributes to handle Sitecore form submissions with appropriate model validation

---
+++ Jabberwocky.Glass.Autofac.Mvc/Attributes/ValidHttpPostAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace Jabberwocky.Glass.Autofac.Mvc.Attributes
+{
+	public class ValidHttpPostAttribute : ActionMethodSelectorAttribute
+	{
+		private static readonly HttpPostAttribute InnerHttpPostAttribute = new HttpPostAttribute();
+		private static readonly ValidateFormHandlerAttribute InnerFormHandlerAttribute = new ValidateFormHandlerAttribute();
+
+		public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+		{
+			return InnerHttpPostAttribute.IsValidForRequest(controllerContext, methodInfo) && InnerFormHandlerAttribute.IsValidForRequest(controllerContext, methodInfo);
+		}
+	}
+}
---
+++ Jabberwocky.Glass.Autofac.Mvc/Util/CustomSitecoreHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Jabberwocky.Glass.Autofac.Mvc.Attributes;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Helpers;
+
+namespace Jabberwocky.Glass.Autofac.Mvc.Util
+{
+	public class CustomSitecoreHelper : SitecoreHelper
+	{
+		public CustomSitecoreHelper(HtmlHelper htmlHelper) : base(htmlHelper)
+		{
+		}
+
+		public override HtmlString FormHandler(string controller, string action)
+		{
+			if (controller.IsEmptyOrNull())
+				controller = GetValueFromCurrentRendering("Controller");
+			if (action.IsEmptyOrNull())
+				action = GetValueFromCurrentRendering("Controller Action");
+			if (controller.IsEmptyOrNull())
+				return new HtmlString(string.Empty);
+
+			string str = HtmlHelper.Hidden(ValidateFormHandlerAttribute.FormHandlerControllerHiddenInput, controller).ToString();
+			if (!action.IsEmptyOrNull())
+				str += HtmlHelper.Hidden(ValidateFormHandlerAttribute.FormHandlerActionHiddenInput, action).ToString();
+
+			return new HtmlString(str);
+		}
+	}
+}
