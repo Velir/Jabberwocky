754e478f8a360b5d5afe5b111564e03f9181880e
Chris Smith <chris.smith@velir.com>
2015-08-21 16:15:45 -0400

    Adding code fix for ensuring Glass Factory Types are abstract

--- Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.Test/UnitTests.cs
+++ Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.Test/UnitTests.cs
@@ -4,75 +4,13 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using TestHelper;
-using Jabberwocky.Glass.CodeAnalysis;
 
 namespace Jabberwocky.Glass.CodeAnalysis.Test
 {
 	[TestClass]
 	public class UnitTest : CodeFixVerifier
 	{
-
-		//No diagnostics expected to show up
-		//[TestMethod]
-		public void TestMethod1()
-		{
-			var test = @"";
-
-			VerifyCSharpDiagnostic(test);
-		}
-
-		//Diagnostic and CodeFix both triggered and checked for
-		//[TestMethod]
-		public void TestMethod2()
-		{
-			var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TypeName
-        {   
-        }
-    }";
-			var expected = new DiagnosticResult
-			{
-				Id = "JabberwockyGlassCodeAnalysis",
-				Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-				Severity = DiagnosticSeverity.Warning,
-				Locations =
-					new[] {
-							new DiagnosticResultLocation("Test0.cs", 11, 15)
-						}
-			};
-
-			VerifyCSharpDiagnostic(test, expected);
-
-			var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-			VerifyCSharpFix(test, fixtest);
-		}
-
-		[TestMethod]
-		public void GlassFactoryType_ImplementingClass_IsAbstract()
-		{
-			var code = @"
+		private const string GlassFactoryType_NotAbstract_TestBadSource = @"
 
 using System;
 using System.Collections.Generic;
@@ -102,6 +40,41 @@ public interface IBaseType : IGlassBase
 }
 
 ";
+
+		private const string GlassFactoryType_NotAbstract_TestFixedSource = @"
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Jabberwocky.Glass.Factory;
+using Jabberwocky.Glass.Factory.Attributes;
+using Jabberwocky.Glass.Factory.Implementation;
+using Jabberwocky.Glass.Factory.Interceptors;
+using Jabberwocky.Glass.Factory.Util;
+using Jabberwocky.Glass.Models;
+
+[GlassFactoryType(typeof (IBaseType))]
+public /*abstract*/ abstract class IBaseTypeModel : ITestInterface
+{
+	
+}
+
+[GlassFactoryInterface]
+public interface ITestInterface {
+}
+
+public interface IBaseType : IGlassBase
+{
+}
+
+";
+
+		[TestMethod]
+		public void GlassFactoryType_ImplementingClass_IsAbstract_Analysis()
+		{
 			var expected = new DiagnosticResult
 			{
 				Id = GlassFactoryTypeIsAbstractAnalyzer.DiagnosticId,
@@ -113,9 +86,15 @@ public interface IBaseType : IGlassBase
 						}
 			};
 
-			VerifyCSharpDiagnostic(code, expected);
+			VerifyCSharpDiagnostic(GlassFactoryType_NotAbstract_TestBadSource, expected);
 
 
+		}
+
+		[TestMethod]
+		public void GlassFactoryType_ImplementingClass_IsAbstract_Fix()
+		{
+			VerifyCSharpFix(GlassFactoryType_NotAbstract_TestBadSource, GlassFactoryType_NotAbstract_TestFixedSource);
 		}
 
 		protected override CodeFixProvider GetCSharpCodeFixProvider()
--- Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/CodeFixProvider.cs
+++ Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/CodeFixProvider.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
 using System.Threading;
@@ -10,20 +8,15 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
 
 namespace Jabberwocky.Glass.CodeAnalysis
 {
 	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(JabberwockyGlassCodeAnalysisCodeFixProvider)), Shared]
 	public class JabberwockyGlassCodeAnalysisCodeFixProvider : CodeFixProvider
 	{
-		private const string title = "Make uppercase";
+		private const string Title = "Make abstract";
 
-		public sealed override ImmutableArray<string> FixableDiagnosticIds
-		{
-			get { return ImmutableArray.Create(GlassFactoryTypeIsAbstractAnalyzer.DiagnosticId); }
-		}
+		public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(GlassFactoryTypeIsAbstractAnalyzer.DiagnosticId);
 
 		public sealed override FixAllProvider GetFixAllProvider()
 		{
@@ -34,36 +27,34 @@ namespace Jabberwocky.Glass.CodeAnalysis
 		{
 			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-			// TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
 			var diagnostic = context.Diagnostics.First();
 			var diagnosticSpan = diagnostic.Location.SourceSpan;
 
 			// Find the type declaration identified by the diagnostic.
-			var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+			var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().FirstOrDefault(); /* TypeDeclarationSyntax */
+
+			// Only operate on class declarations explicitly
+			if (declaration == null) return;
 
 			// Register a code action that will invoke the fix.
 			context.RegisterCodeFix(
 				CodeAction.Create(
-					title: title,
-					createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
-					equivalenceKey: title),
+					title: Title,
+					createChangedSolution: c => MakeAbstractAsync(context.Document, declaration, c),
+					equivalenceKey: Title),
 				diagnostic);
 		}
 
-		private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+		private async Task<Solution> MakeAbstractAsync(Document document, ClassDeclarationSyntax typeDecl, CancellationToken cancellationToken)
 		{
-			// Compute new uppercase name.
-			var identifierToken = typeDecl.Identifier;
-			var newName = identifierToken.Text.ToUpperInvariant();
+			var newModifiers = SyntaxFactory.TokenList(typeDecl.Modifiers.Concat(new[] { SyntaxFactory.Token(SyntaxKind.AbstractKeyword) }));
+			var newTypeDecl = typeDecl.WithModifiers(newModifiers);
+			
+			var root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
 
-			// Get the symbol representing the type to be renamed.
-			var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-			var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-			// Produce a new solution that has all references to that type renamed, including the declaration.
-			var originalSolution = document.Project.Solution;
-			var optionSet = originalSolution.Workspace.Options;
-			var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+			var newRoot = root.ReplaceNode(typeDecl, newTypeDecl);
+			var newDocument = document.WithSyntaxRoot(newRoot);
+			var newSolution = newDocument.Project.Solution;
 
 			// Return the new solution with the now-uppercase type name.
 			return newSolution;
