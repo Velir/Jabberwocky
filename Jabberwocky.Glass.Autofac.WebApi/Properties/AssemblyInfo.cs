1514bb996d8cb1e26b7866ebb5ee8a9b8400e9a1
Chris Smith <chris.smith@velir.com>
2016-09-20 16:39:32 -0400

    Updated major version numbers for all projects

--- Jabberwocky.Glass.Autofac.WebApi/Properties/AssemblyInfo.cs
+++ Jabberwocky.Glass.Autofac.WebApi/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Jabberwocky.Glass.Autofac.WebApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Velir")]
 [assembly: AssemblyProduct("Jabberwocky.Glass.Autofac.WebApi")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Velir 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
