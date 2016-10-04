d7fe730086d7f77e330cc263da439c8b4bd186ae
Kevin Mazzoni <kevin.mazzoni@velir.com>
2016-10-04 10:19:10 -0400

    Adding Extras project for New Relic customizations for Sitecore.

---
+++ src/Extras/Jabberwock.Extras.NewRelic.Sc/Pipelines/HttpRequestBegin/TransactionNameProcessor.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Pipelines.HttpRequest;
+using NR = NewRelic; 
+
+namespace Jabberwocky.Extras.NewRelic.Sc.Pipelines.HttpRequestBegin
+{
+	public class TransactionNameProcessor : HttpRequestProcessor
+	{
+		public override void Process(HttpRequestArgs args)
+		{
+			Item currentItem = Sitecore.Context.Item;
+			if (!string.IsNullOrEmpty(currentItem?.TemplateName))
+			{
+				NR.Api.Agent.NewRelic.SetTransactionName("Webpage", currentItem.TemplateName);
+			}
+		}
+	}
+}
---
+++ src/Extras/Jabberwock.Extras.NewRelic.Sc/Reference/Constants.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Jabberwocky.Extras.NewRelic.Sc.Reference
+{
+	public class Constants
+	{
+		public const string NewRelicBeginStartTimeKey = "NewRelicRenderingStopwatch";
+		public const string NewRelicAppNameSetting = "NewRelic.AppName";
+	}
+}
