93ee742d345739ffe29ac02b75209883e5e65f92
Chris Smith <chris.smith@velir.com>
2016-10-07 14:42:17 -0400

    Updating the nuget bat file to automatically discover candidate nuget packages to be built

--- tools/Nuget/nuget.bat
+++ tools/Nuget/nuget.bat
@@ -1,15 +1,9 @@
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.Core/Jabberwocky.Core.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.DependencyInjection/Jabberwocky.DependencyInjection.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.Glass/Jabberwocky.Glass.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.Glass.Autofac/Jabberwocky.Glass.Autofac.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.Glass.Mvc/Jabberwocky.Glass.Mvc.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.Glass.Autofac.Mvc/Jabberwocky.Glass.Autofac.Mvc.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.Glass.Autofac.WebApi/Jabberwocky.Glass.Autofac.WebApi.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.WebApi/Jabberwocky.WebApi.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.WebApi.Sc/Jabberwocky.WebApi.Sc.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Jabberwocky.Autofac/Jabberwocky.Autofac.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Extras\Jabberwocky.Autofac.Extras.Log4Net.Sc/Jabberwocky.Autofac.Extras.Log4Net.Sc.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Extras\Jabberwocky.Autofac.Extras.MiniProfiler/Jabberwocky.Autofac.Extras.MiniProfiler.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\src\Extras\Jabberwocky.Autofac.Extras.MiniProfiler.Sc/Jabberwocky.Autofac.Extras.MiniProfiler.Sc.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
+setlocal enabledelayedexpansion
+for /r ..\..\src\ %%i in (*.nuspec) do  (
+set projfile=%%i
+set projfile=!projfile:nuspec=csproj!
+..\..\.nuget\NuGet.exe pack !projfile! -IncludeReferencedProjects -symbols -Build -OutputDirectory . -Prop Configuration=Release
+)
+
 ..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
 ..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
