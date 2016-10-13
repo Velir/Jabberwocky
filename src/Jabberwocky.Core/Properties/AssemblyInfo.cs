b2e7a34a5b0d2296f8968d38c6d94691dedad062
Chris Smith <chris.smith@velir.com>
2016-10-13 10:46:32 -0400

    Updating assembly versions

--- src/Jabberwocky.Core/Properties/AssemblyInfo.cs
+++ src/Jabberwocky.Core/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha5")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha6")]
 [assembly: InternalsVisibleTo("Jabberwocky.Core.Tests")]
