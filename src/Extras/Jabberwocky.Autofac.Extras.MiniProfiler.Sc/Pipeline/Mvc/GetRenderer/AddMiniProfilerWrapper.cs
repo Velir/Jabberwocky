89f92094a392708e93405c74a68951709e2e29bf
Chris Smith <chris.smith@velir.com>
2016-11-01 14:33:32 -0400

    Added a miniprofiler timing decorator to the getRenderer pipeline

---
+++ src/Extras/Jabberwocky.Autofac.Extras.MiniProfiler.Sc/Pipeline/Mvc/GetRenderer/AddMiniProfilerWrapper.cs
@@ -0,0 +1,21 @@
+﻿using Jabberwocky.Autofac.Extras.MiniProfiler.Sc.Renderer;
+using Jabberwocky.Autofac.Extras.MiniProfiler.Util;
+using Sitecore.Mvc.Pipelines.Response.GetRenderer;
+
+namespace Jabberwocky.Autofac.Extras.MiniProfiler.Sc.Pipeline.Mvc.GetRenderer
+{
+	public class AddMiniProfilerWrapper : GetRendererProcessor
+	{
+		public override void Process(GetRendererArgs args)
+		{
+			if (args.Result == null || !MiniProfilerRuntime.MiniProfilerInitialized) return;
+
+			var rendering = args.Rendering;
+			var renderer = args.Result;
+
+			if (renderer == null) return;
+
+			args.Result = new MiniProfilerRenderer(renderer, rendering);
+		}
+	}
+}
---
+++ src/Extras/Jabberwocky.Autofac.Extras.MiniProfiler.Sc/Renderer/MiniProfilerRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Sitecore.Mvc.Presentation;
+using StackExchange.Profiling;
+using Profiler = StackExchange.Profiling.MiniProfiler;
+
+namespace Jabberwocky.Autofac.Extras.MiniProfiler.Sc.Renderer
+{
+	public class MiniProfilerRenderer : Sitecore.Mvc.Presentation.Renderer
+	{
+		private readonly Sitecore.Mvc.Presentation.Renderer _innerRenderer;
+		private readonly Rendering _rendering;
+
+		public MiniProfilerRenderer(Sitecore.Mvc.Presentation.Renderer innerRenderer, Rendering rendering)
+		{
+			if (innerRenderer == null) throw new ArgumentNullException(nameof(innerRenderer));
+			if (rendering == null) throw new ArgumentNullException(nameof(rendering));
+			_innerRenderer = innerRenderer;
+			_rendering = rendering;
+		}
+
+		public override void Render(TextWriter writer)
+		{
+			using (Profiler.Current.Step($"Rendering:{_rendering.RenderingItem?.Name}"))
+			{
+				_innerRenderer.Render(writer);
+			}
+		}
+	}
+}
