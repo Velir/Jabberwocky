842aafcbe11ae136f7132725e3bfe3a340fcd41e
Chris Smith <chris.smith@velir.com>
2016-11-08 14:30:18 -0500

    Adding a new metapackage for nuget

--- tools/Nuget/nuget.bat
+++ tools/Nuget/nuget.bat
@@ -5,5 +5,7 @@ set projfile=!projfile:nuspec=csproj!
 ..\..\.nuget\NuGet.exe pack !projfile! -IncludeReferencedProjects -symbols -Build -OutputDirectory . -Prop Configuration=Release
 )
 
+..\..\.nuget\NuGet.exe pack ..\..\Jabberwocky.Library.nuspec -NoPackageAnalysis -Version 2.0.0-alpha9-3 -OutputDirectory .
+
 ..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
 ..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
