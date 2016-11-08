6821086bf4c8d3ad90c2bc493bba9e96ad9ad4ef
Chris Smith <chris.smith@velir.com>
2016-11-08 16:07:36 -0500

    Updating assembly versions

--- src/Extras/Jabberwocky.Autofac.Extras.MiniProfiler/Properties/AssemblyInfo.cs
+++ src/Extras/Jabberwocky.Autofac.Extras.MiniProfiler/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha9-3")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha9-4")]
 
 [assembly: InternalsVisibleTo("Jabberwocky.Autofac.Extras.MiniProfiler.Sc")]
