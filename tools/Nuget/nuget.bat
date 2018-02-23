7c588167c69ccb9f28514f596a4a2fc25a10d647
Chris Sulham <chris.sulham@velir.com>
2018-02-23 15:29:36 -0500

    Incrementing versions for nuget,

--- tools/Nuget/nuget.bat
+++ tools/Nuget/nuget.bat
@@ -2,10 +2,10 @@ setlocal enabledelayedexpansion
 for /r ..\..\src\ %%i in (*.nuspec) do  (
 set projfile=%%i
 set projfile=!projfile:nuspec=csproj!
-..\..\.nuget\NuGet.exe pack !projfile! -IncludeReferencedProjects -symbols -Build -OutputDirectory . -Prop Configuration=Release
+..\..\.nuget\NuGet.exe pack !projfile! -IncludeReferencedProjects -symbols -Build -OutputDirectory .\symbols -Prop Configuration=Release
 )
 
-..\..\.nuget\NuGet.exe pack ..\..\Jabberwocky.Library.nuspec -NoPackageAnalysis -Version 2.0.0-alpha9-3 -OutputDirectory .
+..\..\.nuget\NuGet.exe pack ..\..\Jabberwocky.Library.nuspec -NoPackageAnalysis -Version 2.0.1.0 -OutputDirectory .
 
-..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
-..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
+..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis.nuspec -NoPackageAnalysis -OutputDirectory . -Prop Configuration=Release
+..\..\.nuget\NuGet.exe pack ..\..\diagnostics\Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.nuspec -NoPackageAnalysis -OutputDirectory . -Prop Configuration=Release
